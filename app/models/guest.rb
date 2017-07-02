084351f4cd91f8e4f043c8eeb0d9921e8b16050e
Dan Bonehill <Danbonehill@me.com>
2017-07-02 16:08:28 +0000

    Add model validations and test

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,2 +1,22 @@
 class Guest < ApplicationRecord
+  before_save :capitalize_name
+  after_initialize :set_defaults, :set_unique_code
+  
+  validates :name, presence: true
+  validates :rsvp, :inclusion => { :in => ["Awaiting RSVP" , "Attending", "Not Attending"] }
+  validates :unique_code, uniqueness: true
+  
+  private
+  
+  def set_defaults
+    self.rsvp = "Awaiting RSVP"
+  end
+  
+  def capitalize_name
+    self.name = name.capitalize
+  end
+  
+  def set_unique_code
+    self.unique_code = 6.times.map { [*'0'..'9'].sample }.join
+  end
 end
--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Guest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @guest = Guest.create(name: "Dan")
+  end
+  
+  it "is valid" do
+    expect(@guest).to be_valid
+  end
+  
+  it "has name attribute" do
+    @guest.name = nil
+    @guest.save
+    
+    expect(@guest).not_to be_valid
+  end
+  
+  it "name is capitalized" do
+    @guest.name = "dan"
+    @guest.save
+    
+    expect(@guest.name).to eq(@guest.name.capitalize)
+  end
+  
+  it "has rsvp of 'Awaiting RSVP'" do
+    expect(@guest.rsvp).to eq("Awaiting RSVP")
+  end
+  
+  it "rsvp can only equal 'Awaiting RSVP', 'Attending' or 'Not Attending'" do
+    @guest.rsvp = "aaaa"
+    @guest.save
+    
+    expect(@guest).not_to be_valid
+  end
+  
+  it "has unique_code" do
+    expect(@guest.unique_code).not_to eq(nil)
+    expect((@guest.unique_code.to_s).length).to eq(6)
+  end
+  
+  it "unique_code is unique" do
+    @guest.unique_code = 123456
+    @guest.save
+    
+    @dupguest = Guest.create(name: "Bob")
+    @dupguest.unique_code = 123456
+    @dupguest.save
+    
+    expect(@dupguest).not_to be_valid
+  end
+  
+  it "is valid with email attribute" do
+    @guest.email = "Dan@example.com"
+    @guest.save
+    
+    expect(@guest).to be_valid
+  end
+  
+  it "is valid without email attribute" do
+    @guest.email = nil
+    @guest.save
+    
+    expect(@guest).to be_valid
+  end
 end
