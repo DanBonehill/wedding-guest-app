7e78c88c2cb0bf3e4281faa1f449becc2486e225
DanBonehill <DanBonehill@me.com>
2017-07-29 23:09:01 +0530

    Add Contact form and Message index & show
    Add link to messages index to navigation along with badge displaying unread messages
    Add content to Message index, show and new views
    Add actions to Message controller
    Add custom styling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,28 +1,45 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show]
+  before_action :require_user, only: [:index, :show]
 
   def index
-
+    @messages = Message.all
   end
 
   def show
-
+    set_read_true
+    @invitation = Invitation.find_by(unique_code: @message.unique_code) || nil
   end
 
   def create
-
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:success] = "Thank you for your message. We'll be in touch shortly."
+      redirect_to info_path
+    else
+      flash.now[:danger] = "Something went wrong"
+      render :new
+    end
   end
 
   def new
-
+    @message = Message.new
   end
 
   private
 
-  def  message_params
-
+  def message_params
+    params.require(:message).permit(:name, :email, :content, :unique_code, :read)
   end
 
   def set_message
+    @message = Message.find(params[:id])
+  end
 
+  def set_read_true
+    if @message.read != true
+      @message.read = true
+      @message.save
+    end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
+  before_validation :set_defaults, on: :create
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, length: { maximum: 255 },
@@ -6,5 +7,12 @@ class Message < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true, length: { maximum: 255 }
   validates :unique_code, length: { is: 6 }, allow_blank: true
-  validates :read, presence: true
+
+  scope :unread, -> { where(read: false) }
+
+  private
+
+  def set_defaults
+    self.read = false
+  end
 end
