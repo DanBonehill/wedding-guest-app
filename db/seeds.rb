b93b4d701e2955031c82818ab5a8ea71b64f10dc
DanBonehill <DanBonehill@me.com>
2017-07-29 23:05:45 +0530

    Add to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create User
+User.create(email: "danbonehill@me.com", password: "Password")
+
+# Create Invitations
+invitations = Invitation.create([{}, {}, {}, {}])
+
+# Create Guests
+Guest.create([
+    { name: "Daniel Bonehill", invitation: invitations[0] },
+    { name: "Beth Stevenson", invitation: invitations[1] },
+    { name: "Andrew Bonehill", invitation: invitations[2] },
+    { name: "Michelle Bonehill", invitation: invitations[2] },
+    { name: "Emma Bonehill", invitation: invitations[3] }
+  ])
+
+# Create Messages
+Message.create([
+    { name: "Daniel Bonehill", email: "dan@example.com", content: "This is a seeded message", unique_code: invitations[0][:unique_code], read: false },
+    { name: "Emma Bonehill", email: "emma@example.com", content: "This is a seeded message", unique_code: invitations[3][:unique_code], read: false },
+    { name: "Alan Stevenson", email: "alan@example.com", content: "This is a seeded message", read: false },
+    { name: "Fred", email: "fred@example.com", content: "This is a seeded message", read: false }
+  ])
