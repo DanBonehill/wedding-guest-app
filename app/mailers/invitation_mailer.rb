9c0281626914b15050065d9e8da3e74928063e47
DanBonehill <DanBonehill@me.com>
2017-09-27 21:54:18 +0100

    Add invitation reminder mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -6,4 +6,8 @@ class InvitationMailer < ApplicationMailer
     @url = "http://danbethwedding.com/invitations/#{@invitation.id}"
     mail(to: "danbonehill@me.com", subject: "#{@invitation.guests.first.name} has RSVP'd")
   end
+
+  def reminder_email(invitation)
+    mail(to: "#{invitation.email}", subject: "Dan & Beth's Wedding - Tomorrow!")
+  end
 end
